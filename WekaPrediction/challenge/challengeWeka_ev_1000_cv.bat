435e2b445bad5713c9ad6077e35adc059dfec79b
Rehana Begam <rehan.sheta@gmail.com>
2016-11-19 23:44:51 -0400

    Add files via upload

---
+++ WekaPrediction/challenge/challengeWeka_ev_1000_cv.bat
@@ -0,0 +1,39 @@
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_11_data_1.arff -x 10 -p 1 -distribution -- -C 0.1 > ev_1000\TF_11_data_1_cv_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_11_data_1.arff -x 10 -i -- -C 0.1 > ev_1000\TF_11_data_1_cv_stat.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_12_data_1.arff -x 10 -p 1 -distribution -- -C 0.1 > ev_1000\TF_12_data_1_cv_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_12_data_1.arff -x 10 -i -- -C 0.1 > ev_1000\TF_12_data_1_cv_stat.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_13_data_1.arff -x 10 -p 1 -distribution -- -C 0.1 > ev_1000\TF_13_data_1_cv_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_13_data_1.arff -x 10 -i -- -C 0.1 > ev_1000\TF_13_data_1_cv_stat.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_14_data_1.arff -x 10 -p 1 -distribution -- -C 0.1 > ev_1000\TF_14_data_1_cv_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_14_data_1.arff -x 10 -i -- -C 0.1 > ev_1000\TF_14_data_1_cv_stat.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_15_data_1.arff -x 10 -p 1 -distribution -- -C 0.1 > ev_1000\TF_15_data_1_cv_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_15_data_1.arff -x 10 -i -- -C 0.1 > ev_1000\TF_15_data_1_cv_stat.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_16_data_2.arff -x 10 -p 1 -distribution -- -C 0.1 > ev_1000\TF_16_data_2_cv_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_16_data_2.arff -x 10 -i -- -C 0.1 > ev_1000\TF_16_data_2_cv_stat.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_17_data_2.arff -x 10 -p 1 -distribution -- -C 0.1 > ev_1000\TF_17_data_2_cv_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_17_data_2.arff -x 10 -i -- -C 0.1 > ev_1000\TF_17_data_2_cv_stat.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_18_data_2.arff -x 10 -p 1 -distribution -- -C 0.1 > ev_1000\TF_18_data_2_cv_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_18_data_2.arff -x 10 -i -- -C 0.1 > ev_1000\TF_18_data_2_cv_stat.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_19_data_2.arff -x 10 -p 1 -distribution -- -C 0.1 > ev_1000\TF_19_data_2_cv_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_19_data_2.arff -x 10 -i -- -C 0.1 > ev_1000\TF_19_data_2_cv_stat.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_20_data_2.arff -x 10 -p 1 -distribution -- -C 0.1 > ev_1000\TF_20_data_2_cv_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_20_data_2.arff -x 10 -i -- -C 0.1 > ev_1000\TF_20_data_2_cv_stat.weka
---
+++ WekaPrediction/challenge/challengeWeka_ev_1000_test.bat
@@ -0,0 +1,19 @@
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_11_data_1.arff -T ev_1000\TF_11_data_2_forPred.arff -p 1 -distribution -- -C 0.1 > ev_1000\TF_11_data_2_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_12_data_1.arff -T ev_1000\TF_12_data_2_forPred.arff -p 1 -distribution -- -C 0.1 > ev_1000\TF_12_data_2_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_13_data_1.arff -T ev_1000\TF_13_data_2_forPred.arff -p 1 -distribution -- -C 0.1 > ev_1000\TF_13_data_2_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_14_data_1.arff -T ev_1000\TF_14_data_2_forPred.arff -p 1 -distribution -- -C 0.1 > ev_1000\TF_14_data_2_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_15_data_1.arff -T ev_1000\TF_15_data_2_forPred.arff -p 1 -distribution -- -C 0.1 > ev_1000\TF_15_data_2_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_16_data_2.arff -T ev_1000\TF_16_data_1_forPred.arff -p 1 -distribution -- -C 0.1 > ev_1000\TF_16_data_1_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_17_data_2.arff -T ev_1000\TF_17_data_1_forPred.arff -p 1 -distribution -- -C 0.1 > ev_1000\TF_17_data_1_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_18_data_2.arff -T ev_1000\TF_18_data_1_forPred.arff -p 1 -distribution -- -C 0.1 > ev_1000\TF_18_data_1_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_19_data_2.arff -T ev_1000\TF_19_data_1_forPred.arff -p 1 -distribution -- -C 0.1 > ev_1000\TF_19_data_1_pred.weka
+
+java -cp "C:\Program Files\Weka-3-6\weka.jar" weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.J48 -t ev_1000\TF_20_data_2.arff -T ev_1000\TF_20_data_1_forPred.arff -p 1 -distribution -- -C 0.1 > ev_1000\TF_20_data_1_pred.weka
